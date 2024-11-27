a1869e3c47de5a93468a02d666aaad07fb6d69a8
Radoslaw Kaminski <radoslaw@cheqd.io>
2024-11-27 14:31:39 +0000

    add more cases

--- scenarios/examples/cheqd/example.py
+++ scenarios/examples/cheqd/example.py
@@ -70,6 +70,24 @@ async def update_did(issuer, did, did_document):
     return updated_did_doc
 
 
+async def deactivate_did(issuer, did):
+    """Deactivate a DID on the Cheqd testnet."""
+    did_deactivate_result = await issuer.post(
+        "/did/cheqd/deactivate",
+        json={
+            "did": did,
+            "options": {"network": "testnet"},
+        },
+    )
+
+    assert did_deactivate_result.get("did") == did, "DID mismatch after deactivation."
+    assert (
+        did_deactivate_result.get("did_document_metadata", {}).get("deactivated") is True
+    ), "DID document metadata does not contain deactivated=true."
+
+    print(f"Deactivated DID: {format_json(did_deactivate_result) }")
+
+
 async def create_schema(issuer, did):
     """Create a schema on the Cheqd testnet."""
     schema_create_result = await issuer.post(
@@ -84,11 +102,8 @@ async def create_schema(issuer, did):
         },
     )
 
-    assert (
-        schema_create_result.get("schema_state", {}).get("state") == "finished"
-    ), "Schema state is not finished."
-
     schema_state = schema_create_result.get("schema_state", {})
+    assert schema_state.get("state") == "finished", "Schema state is not finished."
     assert "schema_id" in schema_state, "Key 'schema_id' is missing in schema_state."
 
     schema_id = schema_state.get("schema_id")
@@ -98,6 +113,52 @@ async def create_schema(issuer, did):
 
     print(f"Created schema: {format_json(schema_create_result)}")
     return schema_id
+
+
+async def create_credential_definition(issuer, did, schema_id):
+    """Create a credential definition on the connected datastore."""
+    cred_def_create_result = await issuer.post(
+        "/anoncreds/credential-definition",
+        json={
+            "credential_definition": {
+                "issuerId": did,
+                "schemaId": schema_id,
+                "tag": "default",
+            }
+        },
+    )
+
+    cred_def_state = cred_def_create_result.get("credential_definition_state", {})
+    assert cred_def_state.get("state") == "finished", "Cred def state is not finished."
+    assert (
+        "credential_definition_id" in cred_def_state
+    ), "Key 'credential_definition_id' is missing in credential_definition_state."
+
+    credential_definition_id = cred_def_state.get("credential_definition_id")
+    assert (
+        did in credential_definition_id
+    ), f"credential_definition_id does not contain the expected DID. Expected '{did}' in '{credential_definition_id}'."
+
+    print(f"Created credential definition: {format_json(cred_def_create_result)}")
+    return credential_definition_id
+
+
+async def assert_credential_definitions(issuer, credential_definition_id):
+    """Retrieve all credential definitions and ensure array contain created credential_definition_id."""
+    get_result = await issuer.get("/anoncreds/credential-definitions")
+
+    credential_definition_ids = get_result.get("credential_definition_ids", [])
+    assert (
+        credential_definition_id in credential_definition_ids
+    ), f"credential_definition_ids does not contain the expected credential_definition_id. Expected '{credential_definition_id}' in '{credential_definition_ids}'."
+
+
+async def assert_wallet_dids(issuer, did):
+    """Retrieve all wallet dids and ensure array contain created did."""
+    get_result = await issuer.get("/wallet/did?method=cheqd")
+
+    dids = get_result.get("results", [])
+    assert any(obj.get("did") == did for obj in dids), f"DID {did} not found in array"
 
 
 async def main():
@@ -110,9 +171,13 @@ async def main():
         did = await create_did(issuer)
         did_document = await resolve_did(issuer, did)
         await update_did(issuer, did, did_document)
-        await create_schema(issuer, did)
-
-        # Credential Definition, Deactivate DID
+        schema_id = await create_schema(issuer, did)
+        credential_definition_id = await create_credential_definition(
+            issuer, did, schema_id
+        )
+        await assert_credential_definitions(issuer, credential_definition_id)
+        await assert_wallet_dids(issuer, did)
+        await deactivate_did(issuer, did)
 
 
 if __name__ == "__main__":
